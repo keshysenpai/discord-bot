c49dbfdfd890cff8e567e618fd39a703986a6d75
keshysenpai <88913775+keshysenpai@users.noreply.github.com>
2021-08-14 16:03:07 +1000

    updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./token.json');
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
@@ -14,10 +14,6 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
 	console.log('Ready!');
-});
-
-client.on('interactionCreate', interaction => {
-	console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
 });
 
 client.on('interactionCreate', async interaction => {
