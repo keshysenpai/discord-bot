a1763cf3a4a87600f300d1272f038ea946b51d7e
keshysenpai <88913775+keshysenpai@users.noreply.github.com>
2021-08-14 15:43:43 +1000

    index upadate

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { token } = require('./config.json');
+const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// Place your client and guild ids here
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
 const clientId = '875886083152416770';
 const guildId = '739687559592018000';
 
@@ -32,4 +40,21 @@ const rest = new REST({ version: '9' }).setToken(token);
 	}
 })();
 
-client.login(token);
+client.on('interactionCreate', interaction => {
+	console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	const { commandName } = interaction;
+	if (!client.commands.has(commandName)) return;
+
+try {
+  await client.commands.get(commandName).execute(interaction);
+} catch (error) {
+  console.error(error);
+  return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+}
+});
+
+client.login("ODc1ODg2MDgzMTUyNDE2Nzcw.YRcCew.3iZBM9hmLzpxdhjypAvnwWmq7ho");
