c134ddf113f6214130d0bd12c24ca128e47bd3e6
keshysenpai <88913775+keshysenpai@users.noreply.github.com>
2021-08-14 16:00:30 +1000

    changes

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { token } = require('./config.json');
-const { Client, Collection, Intents } = require('discord.js');
 const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 
-const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
@@ -15,30 +12,9 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
-const clientId = '875886083152416770';
-const guildId = '739687559592018000';
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
-
-const rest = new REST({ version: '9' }).setToken(token);
-
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
-
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
 client.on('interactionCreate', interaction => {
 	console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
@@ -49,12 +25,12 @@ client.on('interactionCreate', async interaction => {
 	const { commandName } = interaction;
 	if (!client.commands.has(commandName)) return;
 
-try {
-  await client.commands.get(commandName).execute(interaction);
-} catch (error) {
-  console.error(error);
-  return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-}
+	try {
+		await client.commands.get(commandName).execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
 client.login(token);
